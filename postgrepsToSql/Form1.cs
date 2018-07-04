2b6b8fe63b3b18fc1a05e7a8fe932c81fa0d53a2
Unknown <i.yazici@gtu.edu.tr>
2018-07-04 04:06:05 +0300

    up

--- postgrepsToSql/Form1.cs
+++ postgrepsToSql/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,11 @@ namespace postgrepsToSql
 {
     public partial class Form1 : Form
     {
+        private ArrayList _columnNames = new ArrayList();
+        private ArrayList _dataTypes = new ArrayList();
+        private ArrayList _datas = new ArrayList();
+        private string _tableName;
+
         public Form1()
         {
             InitializeComponent();
@@ -36,19 +42,33 @@ namespace postgrepsToSql
                                                  );
             richTextBox1.Text += connectionStr + "\r\n";
 
+            // reads all data
+            ReadDb(connectionStr);
+
+            // gets table name
+            GetTableName(connectionStr);
+
+            // gets data types
+            GetDataTypes(connectionStr);
+
+            // copies db
+            Migrate();
+
+        }
+
+        private void ReadDb(string connectionStr)
+        {
             using (var connection = new NpgsqlConnection(connectionStr))
             {
                 connection.Open();
                 richTextBox1.Text += "Connection is successfull\r\n";
 
                 var queryStr = "SELECT * FROM public.\"Customer\"";
-                var queryStr2 = "SELECT column_name, data_type" +
-                                " FROM information_schema.columns" +
-                                " WHERE table_name = 'table_name'";
+
 
                 richTextBox1.Text += queryStr + "\r\n";
 
-                var cmd = new NpgsqlCommand(queryStr2, connection);
+                var cmd = new NpgsqlCommand(queryStr, connection);
                 var dr = cmd.ExecuteReader();
 
                 bool isShown = false;
@@ -60,24 +80,47 @@ namespace postgrepsToSql
                         for (var i = 0; i < dr.FieldCount; ++i)
                         {
                             richTextBox1.Text += dr.GetName(i) + "\t";
+                            _columnNames.Add(dr.GetName(i));
                         }
                         richTextBox1.Text += "\r\n";
                         isShown = true;
                     }
-                    
+
                     for (var i = 0; i < dr.FieldCount; ++i)
                     {
+                        _datas.Add(dr[i]);
                         richTextBox1.Text += dr[i] + "\t";
                     }
 
                     richTextBox1.Text += "\r\n";
                 }
-
+                connection.Dispose();
             }
+        }
 
+
+        private void GetTableName(string connectionStr)
+        {
+            for (int i = 0; i < _datas.Count; ++i)
+                richTextBox1.Text += _datas[i] + "\r\n";
+        }
+
+        private void GetDataTypes(string connectionStr)
+        {
+            for (int i = 0; i < _columnNames.Count; ++i)
+            {
+                var queryDataType = "SELECT data_type" +
+                                    " FROM information_schema.columns   " +
+                                    " WHERE table_schema = 'public'" +
+                                    " AND table_name =" + "'" + "Customer" + "'" +
+                                    " AND column_name =" + "'" + _columnNames[i] + "'";
+            }
+        }
+
+        private void Migrate()
+        {
             // connect to sql sevrver
-            string connectionStrSql = "Data Source = ; Initial Catalog = STAJDB; Integrated Security = True" ;
-
+            string connectionStrSql = "Data Source = ; Initial Catalog = STAJDB; Integrated Security = True";
         }
     }
 }
